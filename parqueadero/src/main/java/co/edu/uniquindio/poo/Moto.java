6986b149863d0448469fd523d75abd492eb5a403
Santiago Benavides González <santiago.benavidesg1@gmail.com>
2024-05-25 15:10:46 -0500

    Avance en la clase vehiculo y sus heredadas.

--- parqueadero/src/main/java/co/edu/uniquindio/poo/Moto.java
+++ parqueadero/src/main/java/co/edu/uniquindio/poo/Moto.java
@@ -64,46 +64,6 @@ public class Moto extends Vehiculo {
         Moto moto = new Moto(hibrida, placa, modelo, propietario, velMax);
     }
 
-    /**
-     * Método para determinar el tipo de la moto.
-     */
-
-    public static boolean determinarTipo(){
-
-        
-        boolean aux = false;
-
-        /**
-         * Ciclo para asegurarse que el usuario ingrese un dato valido.
-         */
-        while (!aux){
-            System.out.println("Por favor ingrese una opción:");
-            System.out.println("1. Hibrida.          2. Clásica.");
-            int select = scanner.nextInt();
-
-            /**
-             * Switch para la seleccion del tipo de moto.
-             */
-            switch (select) {
-                case 1:
-                    System.out.println("Ha seleccionado su moto como hibrida.");
-                    hibrida = true;
-                    aux = true;
-                    break;
-                
-                case 2:
-                    System.out.println("Ha seleccionado su moto como clásica.");
-                    hibrida = false;
-                    aux = true;
-                    break;
-            
-                default:
-                    System.out.println("No es válido. Intente nuevamente:");
-                    break;
-            }
-        }
-        return hibrida;
-    }
 
 /**
  * Metodos gets
