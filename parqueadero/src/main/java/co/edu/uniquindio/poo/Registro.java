9e39871c6de5243d4a70ebc56534d5ba144bf8cb
Brandon <brandons.gilv@uqvirtual.edu.co>
2024-05-25 19:25:01 -0500

    yeah

--- parqueadero/src/main/java/co/edu/uniquindio/poo/Registro.java
+++ parqueadero/src/main/java/co/edu/uniquindio/poo/Registro.java
@@ -1,5 +1,20 @@
 package co.edu.uniquindio.poo;
 
+import java.time.LocalDateTime;
+
 public class Registro {
+
+    private Vehiculo vehiculo;
+    private LocalDateTime horaIngreso;
+    private int fila,columna;
+
+    public Registro(Vehiculo vehiculo,LocalDateTime horaIngreso, int fila, int columna){
+        this.vehiculo=vehiculo;
+        this.horaIngreso=horaIngreso;
+        this.fila=fila;
+        this.columna=columna;
+    }
+
+    
 
 }
